7ea2a9dcb72c1185942b4f8773d518d7d73a8813
Sowmiya <sowmiyanp12@gmail.com>
2021-03-02 14:33:04 +0530

    Item detail screen, added comments

---
+++ AssignmentSaltSide/ItemDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ItemDetailViewController.swift
+//  AssignmentSaltSide
+//
+//  Created by CSS on 02/03/21.
+//
+
+import UIKit
+
+class ItemDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ AssignmentSaltSide/Utility/UIViewController+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIViewController+Extension.swift
+//  AssignmentSaltSide
+//
+//  Created by CSS on 02/03/21.
+//
+
+import Foundation
